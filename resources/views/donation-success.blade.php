4fd48656acca68fc078b357e047ce98d6b313b80
MarcusAfolabi <biormarcus@gmail.com>
2024-07-19 23:09:31 +0100

    update

--- resources/views/donation-success.blade.php
+++ resources/views/donation-success.blade.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
+@section('title', 'Thank you for your donation')
+@section('main')
+<section class="section-breadcrumb">
+    <div class="relative z-10">
+        <div class="container">
+            <div class="breadcrumb-wrapper">
+                <h1 class="breadcrumb-title">@yield('title')</h1>
+                <p>{{ session('message') }}</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Donation Success</title>
-</head>
+            </div>
+        </div>
+        <div class="absolute inset-0 -z-10">
+            <img src="assets/img/images/th-1/breadcrumb.jpg" alt="breadcrumb" width="1920" height="421" class="h-full w-full object-cover" />
+        </div>
+    </div>
+</section>
 
-<body>
-    <h1>Thank you for your donation!</h1>
-    <p>{{ session('message') }}</p>
-</body>
-
-</html>
+@endsection
