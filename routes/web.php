4e075708ebb6063e60d182742293d8a236446a79
MarcusAfolabi <biormarcus@gmail.com>
2024-09-11 10:57:37 +0300

    updated the search result

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,20 @@ use App\Http\Controllers\WebhookController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/terms', function () {
+    return view('terms');
+});
 Route::get('/policy', function () {
     return view('policy');
 });
-Route::get('/thank-you', function () {
-    return view('pages.thank-you');
-});
+
 Route::get('/about', function () {
     return view('pages.about');
 });
 Route::get('/contact', function () {
     return view('pages.contact');
 });
-Route::get('/donate', function () {
-    return view('pages.donate');
-});
-Route::get('/partner', function () {
-    return view('pages.partner');
-});
+
 
 Route::get('/become-a-christian', function () {
     return view('pages.become-a-christian');
@@ -51,15 +47,26 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/donation-success', function () {
+        return view('donation-success');
+    })->name('donation.success');
+
+    Route::get('/donation-failure', function () {
+        return view('donation-failure');
+    })->name('donation.failure');
+
+    Route::get('/donate', function () {
+        return view('pages.donate');
+    });
+    Route::get('/partner', function () {
+        return view('pages.partner');
+    });
+
+    Route::get('/thank-you', function () {
+        return view('pages.thank-you');
+    });
 });
 
 Route::post('/webhook/flutterwave', [WebhookController::class, 'handleWebhook'])->name('webhook.flutterwave');
 Route::get('/webhook/flutterwave', [WebhookController::class, 'handleRedirect'])->name('webhook.flutterwave.get');
-
-Route::get('/donation-success', function () {
-    return view('donation-success');
-})->name('donation.success');
-
-Route::get('/donation-failure', function () {
-    return view('donation-failure');
-})->name('donation.failure');
